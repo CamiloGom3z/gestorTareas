dc6a11d1ff4cc03b7e5a3df92ed57fb093b4acf0
Camilo Gomez <elcanserbero450@gmail.com>
2024-08-08 07:32:59 -0500

    gestor tareas

---
+++ src/components/Pages/Home/Home.jsx
@@ -0,0 +1,35 @@
+
+import React, { useContext } from "react";
+import { Header } from "../../Layouts/Header/Header";
+import { ContainerTasks } from "../../Layouts/ContainerTasks/ContainerTasks";
+import { Main } from "../../Layouts/Main/Main";
+import { NewTask } from "../../NewTask/NewTask";
+import { taskContext } from "../../Context/Context";
+import { ItemTask } from "../../ItemTask/ItemTask";
+import { FilterTasks } from "../../FilterTasks/FilterTasks";
+
+export const Home = () => {
+  const { filteredTasks } = useContext(taskContext);
+
+  return (
+    <>
+      <Header>
+        <h1>TASKS</h1>
+      </Header>
+      <Main>
+        <NewTask />
+        <FilterTasks />
+        <ContainerTasks>
+          {filteredTasks.map((task) => (
+            <ItemTask
+              idTask={task.id}
+              key={task.id}
+              titleTask={task.title}
+              content={task.description}
+            />
+          ))}
+        </ContainerTasks>
+      </Main>
+    </>
+  );
+};
